d92b77929218e3728783ccf1d1cae36d067aa530
m.countryside <mk.hcj2@gmail.com>
2023-04-08 00:27:18 +0900

    updated css

--- app.js
+++ app.js
@@ -2,26 +2,57 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.listen(port, ()=>{
-    console.log(`Server has been activated at port ${port}.`);
-})
+app.set("view engine", "ejs");
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 
 const https = require('https');
 
-let ejs = require('ejs');
-let people = ['George', 'Bill', 'Kane'];
-let html = ejs.render('<%= people.join(", "); %>', {people: people});
+app.use(express.static('public'))
 
-
+let itemsArr =['hi'];
 
 app.get('/', (req, res)=>{
-    let todayDate = Date();
-    let marker = todayDate.includes("Wed")? 'Today is Wednesday': 'Today is not Wednesday';
-    console.log(typeof(todayDate)+todayDate);
-    res.send(marker);
+    app.use(express.static('public'))
 
+    const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let todayDate = new Date();
+    let todayDay = todayDate.getDay();
+    
+    let weekCheck = weekdayCheck(todayDay);
+    
+    let marker = week[todayDay];
+    console.log(todayDay+" "+todayDate);
+
+    res.render("toDoList", {
+        dayOfWeek: marker,
+        weekLogger: weekCheck,
+        addedItem: itemsArr,
+    });
 });
 
+
+app.post('/', (req, res)=>{
+
+    let item = req.body.newItem;
+    itemsArr.push(item);
+
+
+    res.redirect("/");
+    console.log(itemsArr);
+});
+
+
+app.listen(port, ()=>{
+    console.log(`Server has been activated at port ${port}.`);
+})
+
+
+function weekdayCheck(day) {
+    if(day === 0 || day === 6){
+        return 'weekend';
+    } else {
+        return 'weekday';
+    };
+};
